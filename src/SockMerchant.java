a5f3d424f00e49b4af462c3ca4601d046f444c2e
Purushottam <purushottamn8@gmail.com>
2020-07-17 09:39:16 +0530

    Create SockMerchant.java

---
+++ src/SockMerchant.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class SockMerchant {
+
+    static int pairCount(int[] ar) {
+        HashSet<Integer> unmatched = new HashSet<>();
+        int pairs = 0;
+
+        for(int i = 0; i < ar.length; i++) {
+            //this check the next color and if that matched it'll remove that color and increse the no of pair
+            if(!unmatched.add(ar[i])) {
+                unmatched.remove(ar[i]);
+                pairs++;
+            }
+        }
+        return pairs;
+    }
+    public static void main(String ... arg){
+        System.out.println(pairCount(new int[]{2,3,3,4,2,1}));
+    }
+}
